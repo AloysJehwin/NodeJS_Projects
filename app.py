05a427730c281fdd23f5a3a9f3365dc6f0789f6a
AloysJehwin <aloysjehwin@gmail.com>
2025-03-01 17:29:58 +0530

    Port_Update

--- app.py
+++ app.py
@@ -95,4 +95,4 @@ def update_graph():
     return jsonify({"status": "success"})  # Response to trigger JS refresh
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=2500)
